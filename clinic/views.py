0ebb6dd393b2220621099a82ef28e77cf02a683e
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2024-06-03 08:33:47 +0800

    update

--- clinic/views.py
+++ clinic/views.py
@@ -100,7 +100,7 @@ def Home(request):
 
     for i in patient:
         user_check = Patient.objects.filter(document_id=i['id'])
-        # print("user", user_check)
+        print(i)
         if not user_check:
             user = CustomUser.objects.create(username=i['docData']['email'], password=i['docData']['password'], is_patient=True)
 
